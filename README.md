a7a655988f60277e661fef30cb4869c37f1f7d02
Asher Foster <me@asherfoster.com>
2017-01-01 18:28:41 +1300

    Initial. Added two school things

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SchoolWork
 Sometimes school requires me to make stuff. This is that stuff
+
+## Why is this so dull?
+Since when is school stuff supposed to be interesting?
---
+++ science-1-15/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by asher on 8/08/16.
+ */
+var info = document.getElementById('boring');
+var infoTrigger = document.getElementById('toggle-boring');
+var catcher = document.getElementById('click-catcher');
+var triggered = false;
+catcher.addEventListener("click", function () {
+    info.style.right = '';
+    catcher.style.display = '';
+    triggered = false;
+});
+infoTrigger.addEventListener("click", function () {
+    if (triggered) {
+        info.style.right = '';
+        catcher.style.display = '';
+    }
+    else {
+        info.style.right = '0';
+        setTimeout(function () {
+            catcher.style.display = 'block';
+        }, 1);
+    }
+    triggered = !triggered;
+});
+//# sourceMappingURL=app.js.map
