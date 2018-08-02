8cd1ae30c99e803cff867fa4acd584e4d91c4620
Asher Foster <me@asherfoster.com>
2018-08-02 12:08:52 +1200

    Moved to vue because fuck updating DOM

--- ict-91637/index.js
+++ ict-91637/index.js
@@ -31,98 +31,52 @@ const store = new Store([
         amount: 3
     }
 ]);
-// Top level routing
-let activePage = '';
-function goTo(page) {
-    if (activePage)
-        document.getElementById(activePage).classList.remove('active');
-    document.getElementById(page).classList.add('active');
-    activePage = page;
-}
-goTo(document.getElementsByClassName('view')[0].id);
-// Sales page
-const salesItemsWrapper = document.getElementById('sales-items');
-store.items.forEach(comic => {
-    const item = document.createElement('li');
-    item.appendChild(document.createTextNode(''));
-    item.setAttribute('data-title', comic.title);
-    item.addEventListener('click', function () { sellItem(this); });
-    salesItemsWrapper.appendChild(item);
-});
-function renderSalesPage() {
-    for (let item of salesItemsWrapper.children) {
-        const comic = store.getItem(item.getAttribute('data-title'));
-        item.textContent = `${comic.title} -- ${comic.amount}`;
-    }
-}
-function sellItem(el) {
-    const title = el.getAttribute('data-title');
-    if (store.getItem(title).amount < 1)
-        return queueNotif(`${title} is out of stock`, 3000, 'error');
-    store.sell(title);
-    render();
-    queueNotif(`Sold "${title}"`, 1000);
-}
-// Stock page
-const stockItemWrapper = document.getElementById('stock-items');
-store.items.forEach(comic => {
-    const item = document.createElement('li');
-    const text = document.createElement('p');
-    text.appendChild(document.createTextNode(''));
-    item.appendChild(text);
-    item.setAttribute('data-title', comic.title);
-    stockItemWrapper.appendChild(item);
-});
-function render() {
-    renderSalesPage();
-}
-const notifQueue = [];
 const notifColors = {
     'info': '#4caf50',
     'error': '#f44336'
 };
-let notifActive = false;
-function queueNotif(message, duration = 3000, color = 'info') {
-    notifQueue.push({ message, duration, color: notifColors[color] });
-    if (!notifActive)
-        doNotif(notifQueue.shift());
-}
-function doNotif({ duration, color, message }) {
-    /*
-    - Display notif offscreen
-    - Set timer
-    - Move onscreen
-    - Wait
-    - Move offscreen
-    - Hide
-    * */
-    notifActive = true;
-    const timer = document.getElementById('timer');
-    const notifcation = document.getElementById('notification');
-    const notificationText = document.getElementById('notification-text');
-    // Gets everything ready
-    notifcation.style.display = 'block';
-    notifcation.style.background = color;
-    notificationText.textContent = message;
-    timer.style.width = '100%';
-    timer.style.transition = `linear width ${duration / 1000}s`;
+function doNotif(notif) {
+    // Starts it
+    app.notification = notif;
     setTimeout(() => {
-        // Starts timer, moves notif
-        timer.style.width = '0';
-        notifcation.style.top = 'calc(100vh - 59px)';
-        setTimeout(() => {
-            // Hides notif
-            notifcation.style.top = '100vh';
+        // Ends it
+        app.notification = null;
+        // Queues the next
+        if (app.notifQueue.length) {
             setTimeout(() => {
-                notifcation.style.display = 'none';
-                notifActive = false;
-                if (notifQueue.length) {
-                    setTimeout(() => {
-                        doNotif(notifQueue.shift());
-                    }, 50);
-                }
+                doNotif(app.notifQueue.shift());
             }, 500);
-        }, duration);
-    }, 50);
+        }
+    }, notif.duration);
 }
-render();
+Vue.component('navLink', {
+    props: ['href'],
+    template: `<a @click="navigate" href="#"><slot></slot></a>`,
+    methods: {
+        navigate() {
+            app.page = this.href;
+        }
+    }
+});
+const app = new Vue({
+    el: '#app',
+    data: {
+        store,
+        notification: null,
+        notifQueue: [],
+        page: 'sales'
+    },
+    methods: {
+        sell(title) {
+            if (store.getItem(title).amount < 1)
+                return this.queueNotif(`${title} is out of stock`, 3000, 'error');
+            store.sell(title);
+            this.queueNotif(`Sold "${title}"`, 1000);
+        },
+        queueNotif(message, duration = 3000, color = 'info') {
+            this.notifQueue.push({ message, duration, color: notifColors[color] });
+            if (!this.notification)
+                doNotif(this.notifQueue.shift());
+        }
+    }
+});
--- ict-91637/index.ts
+++ ict-91637/index.ts
@@ -2,7 +2,6 @@ interface Comic {
   title: string;
   amount: number;
 }
-
 class Store {
   public sales = {};
   constructor(
@@ -38,111 +37,61 @@ const store = new Store([
   }
 ]);
 
-
-// Top level routing
-let activePage = '';
-function goTo(page: string) {
-  if(activePage) document.getElementById(activePage).classList.remove('active');
-  document.getElementById(page).classList.add('active');
-  activePage = page;
-}
-goTo(document.getElementsByClassName('view')[0].id);
-
-// Sales page
-const salesItemsWrapper = document.getElementById('sales-items');
-store.items.forEach(comic => {
-  const item = document.createElement('li');
-  item.appendChild(document.createTextNode(''));
-  item.setAttribute('data-title', comic.title);
-  item.addEventListener('click', function(){sellItem(this)});
-  salesItemsWrapper.appendChild(item);
-});
-function renderSalesPage() {
-  for (let item of salesItemsWrapper.children) {
-    const comic = store.getItem(item.getAttribute('data-title'));
-    item.textContent = `${comic.title} -- ${comic.amount}`;
-  }
-}
-function sellItem(el: HTMLElement) {
-  const title = el.getAttribute('data-title');
-  if(store.getItem(title).amount < 1)
-    return queueNotif(`${title} is out of stock`, 3000, 'error');
-  store.sell(title);
-  render();
-  queueNotif(`Sold "${title}"`, 1000)
-}
-
-// Stock page
-const stockItemWrapper = document.getElementById('stock-items');
-store.items.forEach(comic => {
-  const item = document.createElement('li');
-  const text = document.createElement('p');
-  text.appendChild(document.createTextNode(''));
-  item.appendChild(text);
-  item.setAttribute('data-title', comic.title);
-  stockItemWrapper.appendChild(item);
-});
-
-function render() {
-  renderSalesPage()
-}
-
 // Notification
 interface QueuedNotif {
   message: string;
   duration: number;
   color: string;
 }
-const notifQueue: QueuedNotif[] = [];
 const notifColors = {
   'info': '#4caf50',
   'error': '#f44336'
 };
-let notifActive = false;
-function queueNotif(message: string, duration: number = 3000, color: string = 'info') {
-  notifQueue.push({message, duration, color: notifColors[color]});
-  if(!notifActive)
-    doNotif(notifQueue.shift());
-}
-function doNotif({duration, color, message}: QueuedNotif) {
-  /*
-  - Display notif offscreen
-  - Set timer
-  - Move onscreen
-  - Wait
-  - Move offscreen
-  - Hide
-  * */
-  notifActive = true;
-  const timer = document.getElementById('timer');
-  const notifcation = document.getElementById('notification');
-  const notificationText = document.getElementById('notification-text');
-  // Gets everything ready
-  notifcation.style.display = 'block';
-  notifcation.style.background = color;
-  notificationText.textContent = message;
-  timer.style.width = '100%';
-  timer.style.transition = `linear width ${duration/1000}s`;
+function doNotif(notif: QueuedNotif) {
+  // Starts it
+  app.notification = notif;
   setTimeout(() => {
-    // Starts timer, moves notif
-    timer.style.width = '0';
-
-    notifcation.style.top = 'calc(100vh - 59px)';
-    setTimeout(() => {
-      // Hides notif
-      notifcation.style.top = '100vh';
+    // Ends it
+    app.notification = null;
+    // Queues the next
+    if(app.notifQueue.length) {
       setTimeout(() => {
-        notifcation.style.display = 'none';
-        notifActive = false;
-        if(notifQueue.length) {
-          setTimeout(() => {
-            doNotif(notifQueue.shift())
-          }, 50);
-        }
+        doNotif(app.notifQueue.shift())
       }, 500);
-    }, duration);
-  }, 50);
+    }
+  }, notif.duration);
 }
 
+Vue.component('navLink', {
+  props: ['href'],
+  template: `<a @click="navigate" href="#"><slot></slot></a>`,
+  methods: {
+    navigate() {
+      app.page = this.href;
+    }
+  }
+});
 
-render();
+const app = new Vue({
+  el: '#app',
+  data: {
+    store,
+    notification: null,
+    notifQueue: [],
+    page: 'sales'
+  },
+  methods: {
+    sell(title: string) {
+      if(store.getItem(title).amount < 1)
+        return this.queueNotif(`${title} is out of stock`, 3000, 'error');
+      store.sell(title);
+      this.queueNotif(`Sold "${title}"`, 1000);
+    },
+    queueNotif(message: string, duration: number = 3000, color: string = 'info') {
+      this.notifQueue.push({message, duration, color: notifColors[color]});
+      if(!this.notification)
+        doNotif(this.notifQueue.shift());
+    }
+  }
+});
+
